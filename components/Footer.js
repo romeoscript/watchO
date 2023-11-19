d2dbd71ea9a78d7e92c08e8d44468bfa19886c2a
romeoscript <romeobourne211@gmail.com>
2023-11-19 16:52:03 +0100

    done

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className='w-full text-center text-xs text-app-greyish-blue'>
-      <p>Developed with ︎🤍 by Sayan Maity</p>
+      <p>Developed with ︎🤍 by Romeoscript</p>
 
     </footer>
   )
